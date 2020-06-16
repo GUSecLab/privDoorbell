19f3bb068535c0bad13abf000573a162ff5fa779
raririn <fy92@georgetown.edu>
2020-06-16 15:24:26 -0400

    fixed encoding issue

--- RPI/cryptoutils.py
+++ RPI/cryptoutils.py
@@ -4,12 +4,16 @@ from base64 import b64encode
 import random
 import secrets
 
-key = b'281742777473543518207051811201009247628'
-msg = b'Crimson Humble God'
+key = '281742777473543518207051811201009247628'
+msg = 'Crimson Humble God'
 
 class HMACSHA256(object):
 
     def __init__(self, key, data):
+        if isinstance(key, str):
+            key = key.encode()
+        if isinstance(data, str):
+            data = data.encode()
         self.hmac = HMAC.new(key, digestmod=SHA256)
         self.hmac.update(data)
     
@@ -26,16 +30,24 @@ class AESCipher(object):
     GCM_TAG_LENGTH = 16
 
     def __init__(self, key):
+        if isinstance(key, str):
+            key = key.encode()
+
         self.key = key
         self.iv = secrets.token_bytes(AESCipher.GCM_IV_LENGTH)
 
     def encrypt(self, data):
+        if isinstance(data, str):
+            data = data.encode()
+
         cipher = AES.new(self.key, mode=AES.MODE_GCM, nonce=self.iv)
         #cipher.update(data)
         ciphertext, tag = cipher.encrypt_and_digest(data)
         return ciphertext, tag
     
     def encrypt_base64(self, data):
+        if isinstance(data, str):
+            data = data.encode()
         ciphertext, tag = self.encrypt(data)
         return self.bytesToBase64(ciphertext), self.bytesToBase64(tag)
 
