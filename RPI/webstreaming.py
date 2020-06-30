8210e42626c36c1bb16d3b0b7c44bcf07e12b8c4
raririn <fy92@georgetown.edu>
2020-06-30 12:58:26 -0400

    update managetoken page

--- RPI/webstreaming.py
+++ RPI/webstreaming.py
@@ -80,7 +80,7 @@ def bell():
 def manageToken():
     global tokens
     if request.method == 'GET':
-        return render_template("token_management.html", tokens = tokens.getList())
+        return render_template("token_management.html", tokens = tokens)
     elif request.method == 'POST':
         print(request.form.to_dict(), flush=True)
         for k, v in request.form.to_dict().items():
